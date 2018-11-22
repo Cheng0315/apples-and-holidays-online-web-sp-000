fa2b1437d11b41f1187ac344dd97cdc4c574744c
Cheng Vue <cvu3@ucsc.edu>
2018-11-22 03:04:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts str_season.to_s.capitalize + ":"
+    puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
       puts "  #{holiday.to_s.capitalize}: supplies.join(", ")"
     end
