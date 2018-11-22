1a6129df198063c067a54bdc9023314cfb3eeac3
Cheng Vue <cvu3@ucsc.edu>
2018-11-22 03:45:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
      holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        holiday
+        arr.push(holiday)
       end
     end
   end
